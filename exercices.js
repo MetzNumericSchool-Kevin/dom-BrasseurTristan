71dc8627a8de9f65e0c1410583979dd2a66a9925
BrasseurTristan <brasseur.tristan@gmail.com>
2025-02-11 21:56:28 +0100

    Des informations manquent

--- exercices.js
+++ exercices.js
@@ -1,3 +1,9 @@
 // Quel est le titre de la boutique
 const Title = document.querySelector('h1');
-console.log(Title.textContent)
+console.log(Title.textContent);
+
+// Des informations manquent !
+const description_div = document.getElementById('description_boutique');
+const description_p = document.createElement('p');
+description_p.textContent = "Pour cela, il vous suffit de cliquer sur une des potions de la liste ci-dessous pour l'acheter.";
+description_div.appendChild(description_p);
